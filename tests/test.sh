1a9114b3d0707d3f7ae2d7501a898a4605bb7dcd
nastyazz <nastyaz0808@mail.ru>
2024-06-16 20:02:59 +0300

    fix test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -5,5 +5,5 @@ export PG_PORT=38734
 export PG_USER=project
 export PG_PASSWORD=project
 export PG_DBNAME=project_db
-export SECRET_KEY=django-insecure-%=yxp#$#*txi-li*d@=pl5t5415p5_(9hm3ram2lioqx**h&m0
+export SECRET_KEY=django-insecure-%=yxp#$#*txi-li*d@=pl5t5415p5_9hm3ram2lioqx**h&m0
 python3.11 manage.py test $1
